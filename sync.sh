d010bdcc08357f86c64612903f756326dfb2bac1
Ryan Roemer <ryan@loose-bits.com>
2012-04-22 15:59:17 -0400

    Sync script copies now.

--- sync.sh
+++ sync.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-# Check no outstanding changes.
-git status | grep "nothing to commit"
-if [[ $? -ne "0" ]]; then
-  echo "Uncommitted changes."
+SRC_DIR="../node-cakepop/doc"
+
+echo "Check no outstanding changes."
+# git status | grep "nothing to commit"
+# if [[ $? -ne "0" ]]; then
+#   echo "Uncommitted changes."
+#   exit 1
+# fi
+
+echo "Check source destination exists."
+if [[ ! -d "$SRC_DIR" ]]; then
+  echo "Source directory not found."
   exit 1
 fi
+
+echo "Remove existing documentation."
+rm -f *.html
+rm -rf assets
+rm -rf classes
+
+echo "Copy over new documentation."
+cp -rp "${SRC_DIR}//" .
